ad10cf01427341bf2d2e3d21c33e94c646e3c660
Myles <mylesmatthews2112@gmail.com>
2021-10-05 14:49:49 +0000

    1 and 2 Complete

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 //1
-function fizzBuzz(){
-  
+function fizzBuzzz(){
+  for (let i = 1; i<= 100; i++){
+      if (i % 3 === 0 && i % 5 === 0){
+          console.log('Fizzbuzz');
+      } else if (i % 5 === 0) {
+          console.log('Buzz')
+      } else if (i % 3 === 0){
+          console.log('Fizz')
+      } else {
+          console.log(i)
+      }
+  }
 }
-
+//fizzBuzzz()
 //2
 function fizzBuzz(number){
-  
-}
-
+ let NewArray = []
+  for (let i = 1; i <= number; i++){
+      if (i % 3 === 0 && i % 5 === 0){
+          NewArray.push("FizzBuzz")
+      } else if (i % 5 === 0){
+          NewArray.push("Buzz")
+      } else if (i % 3 === 0) {
+          NewArray.push("Fizz")
+          } else {
+              NewArray.push(`${i}`)
+          }
+      }
+      return NewArray
+  }
+console.log(fizzBuzz(10))
 
